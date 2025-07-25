b3fbd9e346504446780f804a9f507f17d6380af5
edpyt <edrsa1321s@gmail.com>
2025-07-25 14:00:08 +0500

    test(auth): add startup application test

--- apps/backend/services/auth_service/tests/integration/test_start_app.py
+++ apps/backend/services/auth_service/tests/integration/test_start_app.py
@@ -1,1 +1,10 @@
-def test_start_without_db() -> None: ...
+import pytest
+from fastapi.testclient import TestClient
+
+
+def test_startup_without_db() -> None:
+    from src.auth_service import app
+
+    with pytest.raises(RuntimeError, match="Can't connect to DB."):
+        with TestClient(app) as client:
+            client.get("/")
