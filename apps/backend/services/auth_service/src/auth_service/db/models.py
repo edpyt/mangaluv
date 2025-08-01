f1847a85da24720d5ef5ea485c00dc7af71b10b7
edpyt <edrsa1321s@gmail.com>
2025-08-01 15:22:23 +0500

    chore(auth): add alembic

---
+++ apps/backend/services/auth_service/src/auth_service/db/models.py
@@ -0,0 +1,28 @@
+"""Authentication service ORM models."""
+
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    """Base ORM model."""
+
+
+class User(Base):
+    """Authentication service user model."""
+
+    __tablename__: str = "users"
+
+    id: Mapped[UUID] = mapped_column(
+        primary_key=True, index=True, default=uuid4
+    )
+    full_name: Mapped[str] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        server_onupdate=func.now(),
+        onupdate=func.now(),
+    )
