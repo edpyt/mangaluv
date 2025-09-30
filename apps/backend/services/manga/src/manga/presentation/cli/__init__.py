3404c7906e8e428d930c5ab276e0861f0367d50c
edpyt <edrsa1321s@gmail.com>
2025-09-30 18:50:19 +0500

    chore(manga,cli): use sqlalchemy table object instead text stmt

--- apps/backend/services/manga/src/manga/presentation/cli/__init__.py
+++ apps/backend/services/manga/src/manga/presentation/cli/__init__.py
@@ -1,31 +1,39 @@
 """Manga service command-line interface."""
 
 from typing import Annotated
+from uuid import uuid4
 
 from rich.prompt import Prompt
-from sqlalchemy import create_engine, text
+from sqlalchemy import MetaData, Table, create_engine, insert
 from typer import Option, Typer, secho
 
 app = Typer()
+metadata = MetaData()
 
 
 @app.command()
 def add_manga(
-    db_uri: Annotated[str, Option(help="Manga service database URI.")],
-    manga_tablename: Annotated[str, Option()] = "manga",
+    db_uri: Annotated[
+        str, Option(help="Manga service database URI.")
+    ] = "postgresql+psycopg://postgres:password@localhost:5432/manga_db",
+    table_name: Annotated[str, Option()] = "manga",
 ):
     """Add new manga to the storage."""
-    with create_engine(db_uri).connect() as con:
+    engine = create_engine(db_uri)
+    table = Table(table_name, metadata, autoload_with=engine)
+    with engine.connect() as con:
         title: str = Prompt.ask(":pencil: Enter manga title")
         description: str = Prompt.ask(":pencil: Enter manga description")
         con.execute(
-            text("INSERT INTO :manga_table VALUES :title,:description"),
-            {
-                "manga_table": manga_tablename,
-                "title": title,
-                "description": description,
-            },
+            insert(table).values(
+                {
+                    "id": uuid4(),  # FIXME: should be default level on db level
+                    "title": title,
+                    "description": description,
+                }
+            )
         )
+
     secho("New manga was added", fg="green")
 
 
